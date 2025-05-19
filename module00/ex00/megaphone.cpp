124f47c7e594d165b640219d94fa4126069a86d7
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-05-19 15:59:51 +0200

    class Megaphone made in OCCF, working as stated in subject

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,17 +1,44 @@
-#include <cstdio>
 #include <iostream>
+#include <string>
+#include <cstring>
 
 class Megaphone
 {
 	public:
-	int test;
+		Megaphone()
+		{
+			;
+		}
+
+		~Megaphone()
+		{
+			;
+		}
+
+		Megaphone(Megaphone& obj)
+		{
+			(void)obj;
+		}
+		
+		void to_upper_string(std::string text)
+		{
+			for (long unsigned int i = 0; i < text.size(); i++)
+			{
+				text[i] = toupper(text[i]);
+			}
+			std::cout << text;
+		}
 };
 
-int main(void)
+int main(int argc, char **argv)
 {
 	Megaphone phone1;
 
-	phone1.test = 1;
-
-	std::cout << phone1.test << '\n';
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << "\n";	
+	for (int i = 1; i < argc; i++)
+	{
+		phone1.to_upper_string(argv[i]);
+	}
+	return (0);
 }
