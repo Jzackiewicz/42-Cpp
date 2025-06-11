cd4a20d5174095332cb0da7b9d21218a091998cd
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-06-11 21:47:30 +0200

    ex06 done

---
+++ module01/ex06/Harl.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Harl {
+    public:
+        Harl();
+        ~Harl();
+
+        void complain(std::string level);
+
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+
+        typedef void (Harl::*HarlFunction)();
+        HarlFunction functions[4];
+        std::string levels[4];
+};
