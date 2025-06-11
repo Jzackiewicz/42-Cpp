6d18b1673146c8b08f42c733b777cf9e8a8ba4ee
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-06-11 21:47:16 +0200

    fix: update error message for argument usage in main function

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -4,8 +4,7 @@ int main(int argc, char **argv)
 {
     if (argc != 4)
     {
-        std::cerr << "Invalid arguments!" << std::endl
-                  << "./swap <filename> <s1> <s2>" << std::endl;
+        std::cerr << "Usage:" << argv[0] << "<filename> <s1> <s2>" << std::endl;
         return (1);
     }
     if (!checkInput(argv[1], argv[2]))
