2bb9dc0690170e06f7145c3393705c618182f2d4
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-06-30 20:43:52 +0200

    rand is not random

--- module05/ex02/RobotomyRequestForm.cpp
+++ module05/ex02/RobotomyRequestForm.cpp
@@ -32,6 +32,8 @@ void RobotomyRequestForm::execute(const Bureaucrat &executor) const
 		if (executor.getGrade() > this->getExecGrade())
 			throw GradeTooLowException();
 		if (rand() % 2)
+			std::cout << "\033[32m" << "Bzzzzzz... " << this->_target << " has been robotomized successfully 50% of the time!" << "\033[0m" << std::endl;
+		else
 			throw RobotomyFailedException();
 	}
 	catch (const FormNotSignedException &e)
@@ -49,7 +51,6 @@ void RobotomyRequestForm::execute(const Bureaucrat &executor) const
 		std::cerr << "\033[31m" << "Caught RobotomyFailedException \033[0m[" << this->getName() << "]: \n" << e.what() << std::endl;
 		return;
 	}
-	std::cout << "\033[32m" << "Bzzzzzz... " << this->_target << " has been robotomized successfully 50% of the time!" << "\033[0m" << std::endl;
 }
 
 const char *RobotomyRequestForm::RobotomyFailedException::what() const throw()
