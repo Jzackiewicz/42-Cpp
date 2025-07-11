01ae7fe61653ad296b12da079832719aa68dd2ea
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-07-11 20:36:45 +0200

    .

--- module08/ex01/Span.cpp
+++ module08/ex01/Span.cpp
@@ -2,6 +2,9 @@
 #include <algorithm>
 #include <stdexcept>
 #include <iostream>
+#include <limits>
+#include <vector>
+#include <iterator>
 
 Span::Span(unsigned int n) : _maxSize(n) {}
 
@@ -47,4 +50,48 @@ void Span::addNumbers(T begin, T end)
         return;
     }
     _numbers.insert(_numbers.end(), begin, end);
+}
+
+unsigned int Span::shortestSpan() const
+{
+    try
+    {
+        {
+            if (_numbers.size() < 2)
+                throw std::runtime_error("Not enough numbers to find a span");
+        }
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    std::vector<int> sortedNumbers = _numbers;
+    std::sort(sortedNumbers.begin(), sortedNumbers.end());
+
+    unsigned int minSpan = UINT_MAX;
+    for (size_t i = 1; i < sortedNumbers.size(); ++i)
+    {
+        unsigned int span = sortedNumbers[i] - sortedNumbers[i - 1];
+        if (span < minSpan)
+            minSpan = span;
+    }
+    return minSpan;
+}
+
+unsigned int Span::longestSpan() const
+{
+    try
+    {
+        if (_numbers.size() < 2)
+            throw std::runtime_error("Not enough numbers to find a span");
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    std::vector<int>::const_iterator minIt = std::min_element(_numbers.begin(), _numbers.end());
+    std::vector<int>::const_iterator maxIt = std::max_element(_numbers.begin(), _numbers.end());
+    return static_cast<unsigned int>(*maxIt - *minIt);
 }
