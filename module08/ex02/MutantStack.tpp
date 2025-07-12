a6f817bc162c2cbe67a21ae1b669a9256e50516d
Jzackiewicz <jj.zackiewicz@gmail.com>
2025-07-12 18:59:37 +0200

    m08 ex02 done

--- module08/ex02/MutantStack.tpp
+++ module08/ex02/MutantStack.tpp
@@ -1,16 +1,16 @@
 #include "MutantStack.hpp"
 
 template <typename T>
-MutantStack<T>::MutantStack(void) : std::stack<int>() {}
+MutantStack<T>::MutantStack(void) : std::stack<T>() {}
 
 template <typename T>
-MutantStack<T>::MutantStack(const MutantStack &other) : std::stack<int>(other) {}
+MutantStack<T>::MutantStack(const MutantStack &other) : std::stack<T>(other) {}
 
 template <typename T>
 MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &other)
 {
     if (this != &other)
-        std::stack<int>::operator=(other);
+        std::stack<T>::operator=(other);
     return (*this);
 }
 
--- module08/ex02/main.cpp
+++ module08/ex02/main.cpp
@@ -1,35 +1,62 @@
-#include <iostream>
 #include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+
+template <typename T>
+void printMutantStack(MutantStack<T> &mstack)
+{
+	for (typename MutantStack<T>::iterator it = mstack.begin(); it != mstack.end(); ++it)
+		std::cout << "{" <<*it << "} ";
+	std::cout << "| Size: " << mstack.size() << std::endl;
+}
+
+template <typename T>
+void printList(std::list<T> &lst)
+{
+	for (typename std::list<T>::iterator it = lst.begin(); it != lst.end(); ++it)
+		std::cout << *it << " ";
+	std::cout << "| Size: " << lst.size() << std::endl;
+}
+
+template <typename T>
+void addArray2Stack(MutantStack<T> &mstack, T *array, size_t size)
+{
+	for (size_t i = 0; i < size; ++i)
+		mstack.push(array[i]);
+}
 
 int main(void)
 {
-	MutantStack<int> mstack;
-
-	mstack.push(5);
-	mstack.push(17);
-
-	std::cout << mstack.top() << std::endl;
-
-	mstack.pop();
-
-	std::cout << mstack.size() << std::endl;
+	MutantStack<int>			mstackInt;
+	MutantStack<float>			mstackFloat;
+	MutantStack<std::string>	mstackString;
+	std::list<int>				lst;
+	int							intArray[] = {5, 17, 3};
+	float						floatArray[] = {1.1f, 0.234234f, -3.349f};
+	std::string					stringArray[] = {"Dupa", "Kościotrupa"};
 	
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
+	std::cout << "MutantStack<int>:" << std::endl;
+	addArray2Stack(mstackInt, intArray, 3);
+	printMutantStack(mstackInt);
+	mstackInt.pop();
+	printMutantStack(mstackInt);
 
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
+	std::cout << std::endl << "MutantStack<float>:" << std::endl;
+	addArray2Stack(mstackFloat, floatArray, 3);
+	printMutantStack(mstackFloat);
 
-	++it;
-	--it;
-	while (it != ite)
-	{
-	std::cout << *it << std::endl;
-	++it;
-	}
-	std::stack<int> s(mstack);
-	return 0;
+	std::cout << std::endl << "MutantStack<std::string>:" << std::endl;
+	addArray2Stack(mstackString, stringArray, 2);
+	printMutantStack(mstackString);
+	mstackString.pop();
+	mstackString.pop();
+	printMutantStack(mstackString);
+
+	std::cout << std::endl << "List<int>:" << std::endl;
+	
+	lst.insert(lst.end(), intArray, intArray + 3);
+	printList(lst);
+	lst.pop_back();
+	printList(lst);
+	return (0);
 }
